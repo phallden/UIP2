018b3c9f136764987ec370f5c29f63d3becc2a44
majkh <holmberg.majk@gmail.com>
2016-04-25 15:26:25 +0200

    fixed preload of images

--- javascript/canvas_objs.js
+++ javascript/canvas_objs.js
@@ -1,45 +1,33 @@
 
 
-var Cloud = function(canvasElem, imageName, widthFact, heightFact,x ,y) {
-
-    // console.log("Ball created");
+var Cloud = function(canvasElem, image, widthFact, heightFact,xFactor ,y) {
 
     // Initial values
     //
     this.widthFact = widthFact;
     this.heightFact = heightFact;
-    this.cloudImage = new Image();
-    this.cloudImage.src = imageName;
+    this.cloudImage = image;
     this.canvas = $('#' + canvasElem);//document.getElementById(canvasElem);
     this.container = $(this.canvas).parent();
     this.ctx = this.canvas.get(0).getContext('2d');
-  ///  var width = $(this.container).width() * widthFact;
-   // var height = width * heightFact;
     this.canvas.attr('width', $(this.container).width()); //max width
-  //  this.canvas.attr('height', height); //max width
-  this.x = x;
-  this.y = y;
-  this.vx = 1;
-  this.vy = 0;
-};
+    this.xFactor = xFactor;
+    this.x = this.canvas.width() * xFactor;
+    this.y = y;
+    this.vx = 1;
+    this.vy = 0;
+  };
 
-Cloud.prototype.start = function () {
-  $this = this;
- // this.ctx.fillStyle = "#FFF"
- // this.ctx.fillRect( 0, 0, this.canvas.width(), this.canvas.height()); //fill the canvas     
-//ct.drawImage(logoImage, 0, 0, c.width(), c.height());
-this.cloudImage.onload = function() {
-  $this.ctx.drawImage( $this.cloudImage, $this.x, $this.y, $this.canvas.width() * $this.widthFact, $this.canvas.width() * $this.widthFact * $this.heightFact)  
-};
+  Cloud.prototype.start = function () {
+    $this = this;
+    this.cloudImage.onload = function() {
+      $this.ctx.drawImage( $this.cloudImage, $this.x, $this.y, $this.canvas.width() * $this.widthFact, $this.canvas.width() * $this.widthFact * $this.heightFact)  
+    };
  // this.ctx.drawImage( this.cloudImage, this.x, this.y, this.canvas.width(), this.canvas.height())  
 };
 
 Cloud.prototype.draw = function () {
- // this.ctx.fillStyle = "#FFF"
- // this.ctx.fillRect( 0, 0, this.canvas.width(), this.canvas.height()); //fill the canvas     
-//ct.drawImage(logoImage, 0, 0, c.width(), c.height());
-this.ctx.drawImage( this.cloudImage, this.x, this.y, this.canvas.width() * this.widthFact, this.canvas.width() * this.widthFact * this.heightFact)
- // this.ctx.drawImage( this.cloudImage, this.x, this.y, this.canvas.width(), this.canvas.height())  
+  this.ctx.drawImage( this.cloudImage, this.x, this.y, this.canvas.width() * this.widthFact, this.canvas.width() * this.widthFact * this.heightFact)
 };
 
 // Adding the tick function. The tick is used to move things (or to provide for other animations).
@@ -63,141 +51,10 @@ Cloud.prototype.tick = function() {
   $(window).resize( respondCanvas );
   function respondCanvas(){ 
     for (k = 0; k < objects.length; ++k) {
-      console.log(objects[k]);
+      objects[k].x = objects[k].canvas.width() * objects[k].xFactor;
       objects[k].canvas.attr('width', $(objects[k].container).width() ); //max width
        objects[k].canvas.attr('height', $(objects[k].container).height() ); //max height
+
      }
-   };
+  };
 
-/**$(document).ready( function(){
-    //Get the canvas & context
-    var c = $('#respondCanvas');
-    var ct = c.get(0).getContext('2d');
-    var container = $(c).parent();
-    var counter = 0;
-        var c2 = $('#respondCanvas2');
-    var ct2 = c2.get(0).getContext('2d');
-    var container2 = $(c2).parent();
-    var counter2 = 0;
-
-    //Run function when browser resizes
-    $(window).resize( respondCanvas );
-
-    function respondCanvas(){ 
-        c.attr('width', $(container).width() ); //max width
-        c.attr('height', $(container).height() ); //max height
-
-        //Call a function to redraw other content (texts, images etc)
-
-  			//Redraw & reposition content
-  			var x = c.width();
-  			var y = c.height();
-  					ct.fillStyle = "#DDDDDD"; //black
-  			ct.fillRect( 0, 0, x, y); //fill the canvas  		
-  			logoImage = new Image(),
-        TO_RADIANS = Math.PI/180, 
-        logoImage.src = '../images/Cloud.png';
-        ct.drawImage(logoImage, 0, 0, c.width(), c.height())	
-
-           c2.attr('width', $(container2).width() ); //max width
-        c2.attr('height', $(container2).height() ); //max height
-
-        //Call a function to redraw other content (texts, images etc)
-
-        //Redraw & reposition content
-        var x2 = c2.width();
-        var y2 = c2.height();
-            ct2.fillStyle = "#DDDDDD"; //black
-        ct2.fillRect( 0, 0, x2, y2); //fill the canvas     
-        logoImage2 = new Image(),
-        TO_RADIANS2 = Math.PI/180, 
-        logoImage2.src = '../images/chicken.png';
-        ct2.drawImage(logoImage2, 0, 0, c2.width(), c2.height())  
-
-  		}
-  		c[0].addEventListener('mouseover', function(e){
-  raf = window.requestAnimationFrame(loop);
-});
-c[0].addEventListener("mouseout",function(e){
-  window.cancelAnimationFrame(raf);
-});
-
-c2[0].addEventListener('mouseover', function(e){
-  raf2 = window.requestAnimationFrame(loop2);
-});
-c2[0].addEventListener("mouseout",function(e){
-  window.cancelAnimationFrame(raf2);
-});
-  		function loop() { 
-  			ct.clearRect(0,0,c.width(), c.height()); 
-  			drawRotatedImage(logoImage,c.width()/2,c.height()/2,counter); 
-  			counter+=2; 
-
-  
-
-  		}
-
-      function loop2(){
-                    ct2.clearRect(0,0,c2.width(), c2.height()); 
-        drawRotatedImage(logoImage2,c2.width()/2,c2.height()/2,counter2); 
-        counter2+=2; 
-      }
-  		function getMousePos(canvas, evt) {
-  			var rect = canvas.getBoundingClientRect();
-  			return {
-  				x: evt.clientX - rect.left,
-  				y: evt.clientY - rect.top
-  			};
-  		};
-
-  		function drawRotatedImage(image, x, y, angle) { 
-
-		// save the current co-ordinate system 
-		// before we screw with it
-		ct.save(); 
-
-		// move to the middle of where we want to draw our image
-		ct.translate(x, y);
-
-		// rotate around that point, converting our 
-		// angle from degrees to radians 
-		ct.rotate(angle * TO_RADIANS);
-
-		// draw it up and to the left by half the width
-		// and height of the image 
-		ct.drawImage(image, -(c.width()/2), -(c.height()/2), c.width(), c.height());
-
-		// and restore the co-ords to how they were when we began
-		ct.restore(); 
-
-        // save the current co-ordinate system 
-    // before we screw with it
-    ct2.save(); 
-
-    // move to the middle of where we want to draw our image
-    ct2.translate(x, y);
-
-    // rotate around that point, converting our 
-    // angle from degrees to radians 
-    ct2.rotate(angle * TO_RADIANS2);
-
-    // draw it up and to the left by half the width
-    // and height of the image 
-    ct2.drawImage(image, -(c2.width()/2), -(c2.height()/2), c2.width(), c2.height());
-
-    // and restore the co-ords to how they were when we began
-    ct2.restore(); 
-	}
-	
-  logoImage = new Image(),
-  TO_RADIANS = Math.PI/180, 
-  logoImage.src = '../images/Cloud.png';
-  ct.drawImage(logoImage, 300, 100, c.width(), c.height())
-    logoImage2 = new Image(),
-  TO_RADIANS2 = Math.PI/180, 
-  logoImage2.src = '../images/chicken.png';
-  ct2.drawImage(logoImage2, 300, 100, c2.width(), c2.height())
-    //Initial call 
-    respondCanvas();
-
-  });  **/
--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,63 @@
 var i = 0
-var objects = [];
+var clouds = [];
+var pigs = [];
+var sources = {
+	Pig: '../images/pig.png',
+	Cloud: '../images/Cloud.png'
+};
+
+function loadImages(sources, callback) {
+	var images = {};
+	var loadedImages = 0;
+	var numImages = 0;
+	// get num of sources
+	for(var src in sources) {
+		numImages++;
+	}
+	for(var src in sources) {
+		images[src] = new Image();
+		images[src].onload = function() {
+			if(++loadedImages >= numImages) {
+				callback(images);
+			}
+		};
+		images[src].src = sources[src];
+	}
+}
+
+
+
 
 function init(){
-	/** main function, init **/
-	for(k=0; k < 3; k++) {
-		objects[i] = new Cloud("respondCanvas", '../images/Cloud.png', (0.1 - k / 30), 1.1, (0 + (k * 140)), (0 + (k * 30)));
-	}
-		
-	// new pig("respondCanvas", '../images/pig.png')
+	loadImages(sources, function(images) {
+		pigs[0] = new Pig("pigCanvas", images.Pig, 0.05, 1.1, 0.25, 200);
+		pigs[1] = new Pig("pigCanvas", images.Pig, 0.05, 1.1, 0.10, 100);
+		pigs[2] = new Pig("pigCanvas", images.Pig, 0.05, 1.1, 0.20, 150);
+		clouds[0] =  new Cloud("cloudCanvas", images.Cloud, 0.1, 1.1, 0.25, 10)
+		clouds[1] =  new Cloud("cloudCanvas", images.Cloud, 0.05, 1.1, 0.1 , 40)
+		clouds[2] =  new Cloud("cloudCanvas", images.Cloud, 0.08, 1.1, 0.4 , 30)
+		/*
+		for(k=0; k <= 7; k++){
+			objects[k] =  new Cloud("cloudCanvas", images.Cloud, (0.1 - k/30), 1.1, 1/(4*k) , (0+(k*30)))
+			Pigs[i] = new Pig("pigCanvas", images.Pig, (0.1 - k/100), 1.1, 1/(2*k), (0+(k*40)));
+			i++;
+		} */
+		for(k=0; k < pigs.length; k++){
 
-	var loopBG = setInterval(function(){
-    // do your thing
-    for (k = 0; k < objects.length; ++k) {
-    	objects[k].clear();
-    }
-    for (k = 0; k < objects.length; ++k) {
-    	objects[k].tick();
-    	objects[k].draw();
-    }
-}, 20);
+			pigs[k].draw();
+		//	objects[k].draw();
+		} 
+		var loopBG = setInterval(function(){
+			for (k = 0; k < clouds.length; ++k) {
+				clouds[k].clear();
+			}
+			for (k = 0; k < clouds.length; ++k) {
+				clouds[k].tick();
+				clouds[k].draw();
+			}
+		}, 20); 
+	});
 }
-$(document).ready( function(){
+window.onload = function() {
 	init();
-});
+};
