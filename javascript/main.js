249896a03aae2bee76586b3c9cae6a237093143c
majkh <holmberg.majk@gmail.com>
2016-04-25 15:47:02 +0200

    sun included in sky canvas, merged fixed

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,10 @@ var clouds = [];
 var pigs = [];
 var sources = {
 	Pig: '../images/pig.png',
-	Cloud: '../images/Cloud.png'
+	Cloud: '../images/Cloud.png',
+	Sun: '../images/sun.png'
 };
-
-<<<<<<< HEAD
+var sunObj;
 function loadImages(sources, callback) {
 	var images = {};
 	var loadedImages = 0;
@@ -37,39 +37,21 @@ function init(){
 		clouds[0] =  new Cloud("cloudCanvas", images.Cloud, 0.1, 1.1, 0.25, 10)
 		clouds[1] =  new Cloud("cloudCanvas", images.Cloud, 0.05, 1.1, 0.1 , 40)
 		clouds[2] =  new Cloud("cloudCanvas", images.Cloud, 0.08, 1.1, 0.4 , 30)
-		/*
-		for(k=0; k <= 7; k++){
-			objects[k] =  new Cloud("cloudCanvas", images.Cloud, (0.1 - k/30), 1.1, 1/(4*k) , (0+(k*30)))
-			Pigs[i] = new Pig("pigCanvas", images.Pig, (0.1 - k/100), 1.1, 1/(2*k), (0+(k*40)));
-			i++;
-		} */
+		sunObj = new Sun("cloudCanvas", images.Sun,0.1, 1.1, 0.85, 20);
+		sunObj.draw();
 		for(k=0; k < pigs.length; k++){
-=======
-function init(){
-	/** main function, init **/
-	for(k=0; k < 3; k++) {
-		objects[i] = new Cloud("respondCanvas", '../images/Cloud.png', (0.1 - k / 30), 1.1, (0 + (k * 140)), (0 + (k * 30)));
-		i++
-
-	}
-
-	new Sun("respondCanvas2", '../images/sun.png',(0.1 - 1 / 30), 1.1, (0 + (1 * 140)), (0 + (1 * 30)));
-	Sun.draw();
->>>>>>> 6c8742d381e02fcdff5e2d06d22b9b240956c288
-
 			pigs[k].draw();
 		//	objects[k].draw();
-		} 
-		var loopBG = setInterval(function(){
-			for (k = 0; k < clouds.length; ++k) {
-				clouds[k].clear();
-			}
-			for (k = 0; k < clouds.length; ++k) {
-				clouds[k].tick();
-				clouds[k].draw();
-			}
-		}, 20); 
-	});
+	} 
+	var loopBG = setInterval(function(){
+		clouds[0].clear();
+		sunObj.draw();
+		for (k = 0; k < clouds.length; ++k) {
+			clouds[k].tick();
+			clouds[k].draw();
+		}
+	}, 20); 
+});
 }
 window.onload = function() {
 	init();
--- javascript/sun.js
+++ javascript/sun.js
@@ -1,59 +1,20 @@
-var Sun = function(canvasElem, imageName, widthFact, heightFact,x ,y) {
-
-    // console.log("Ball created");
+var Sun = function(canvasElem, image, widthFact, heightFact, xFactor, y) {
 
     // Initial values
     //
     this.widthFact = widthFact;
     this.heightFact = heightFact;
-    this.sunImage = new Image();
-    this.sunImage.src = imageName;
+    this.sunImage = image;
     this.canvas = $('#' + canvasElem);//document.getElementById(canvasElem);
     this.container = $(this.canvas).parent();
     this.ctx = this.canvas.get(0).getContext('2d');
-    ///  var width = $(this.container).width() * widthFact;
-    // var height = width * heightFact;
     this.canvas.attr('width', $(this.container).width()); //max width
-    //  this.canvas.attr('height', height); //max width
-    this.x = x;
+    this.x = this.canvas.width() * xFactor;
     this.y = y;
     this.vx = 1;
     this.vy = 0;
 };
 
 Sun.prototype.draw = function () {
-    $this = this;
-    this.sunImage.onload = function() {
-        $this.ctx.drawImage( $this.cloudImage, $this.x, $this.y, $this.canvas.width() * $this.widthFact, $this.canvas.width() * $this.widthFact * $this.heightFact)
-    };
+    this.ctx.drawImage( this.sunImage, this.x, this.y, this.canvas.width() * this.widthFact, this.canvas.width() * this.widthFact * this.heightFact)
 };
-
-
-/*Sun.prototype.tick = function() {
-    this.x += this.vx;
-
-    // Detection of the walls. This is not perfect, so it is left as an exercise to improve it.
-    //
-
-    if (this.x + this.vx > this.canvas.width() || this.x + this.vx < 0) {
-        this.vx = -this.vx;
-        this.x = 0;
-    }
-};
-
-Sun.prototype.clear = function(){
-    this.ctx.clearRect(0, 0, this.canvas.width(), this.canvas.height());
-}*/
-
-
-
-/*
-var canvas = document.getElementById('canvasID');
-var context = canvas.getContext('2d');
-var imageObj = new Image();
-
-imageObj.onload = function() {
-        context.drawImage(imageObj, 69, 50);
-      };
-
-      imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';*/
