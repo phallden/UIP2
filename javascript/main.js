b7e0f4081ee37994ed58782eec1b7fb250205a79
majkh <holmberg.majk@gmail.com>
2016-04-25 12:17:49 +0200

    deleted comments

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@ var objects = [];
 function init(){
 	/** main function, init **/
 	for(k=0; k < 3; k++){
-		objects[i] =  new Cloud("respondCanvas", '../images/Cloud.png', (0.1 - k/30), 1.1, (0+(k*140)), (0+(k*30)));
+		objects[i] =  new Cloud("respondCanvas", '../images/Cloud.png', (0.1 - k/30), 1.1, (0+(k*140)), (k*30));
 		i++;
 	}
 	var loopBG = setInterval(function(){
