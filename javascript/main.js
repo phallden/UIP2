1d1eb9d2df8fa5dc3c6ff3b3d42b29c80f3b8132
phallden <phallden2531@gmail.com>
2016-05-04 13:17:17 +0200

    horse

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,13 @@
 var i = 0
 var clouds = [];
 var pigs = [];
+var horse = [];
 var sources = {
 	Pig: '../images/pig.png',
 	Cloud: '../images/Cloud.png',
 	Sun: '../images/sun.png',
-	Sun2: '../images/chicken.png'
+	Sun2: '../images/chicken.png',
+	Horse: '../images/horse2.jpg'
 };
 var sunObj;
 function loadImages(sources, callback) {
@@ -35,15 +37,19 @@ function init(){
 		pigs[0] = new Pig("pigCanvas", images.Pig, 0.05, 1.1, 0.25, 200);
 		pigs[1] = new Pig("pigCanvas", images.Pig, 0.05, 1.1, 0.10, 100);
 		pigs[2] = new Pig("pigCanvas", images.Pig, 0.05, 1.1, 0.20, 150);
-		clouds[0] =  new Cloud("cloudCanvas", images.Cloud, 0.1, 1.1, 0.25, 10)
-		clouds[1] =  new Cloud("cloudCanvas", images.Cloud, 0.05, 1.1, 0.1 , 40)
-		clouds[2] =  new Cloud("cloudCanvas", images.Cloud, 0.08, 1.1, 0.4 , 30)
+		horse[0] = new Horse("horseCanvas", images.Horse, 0.5, 0.8, 0.50, 20);
+		clouds[0] =  new Cloud("cloudCanvas", images.Cloud, 0.1, 1.1, 0.25, 10);
+		clouds[1] =  new Cloud("cloudCanvas", images.Cloud, 0.05, 1.1, 0.1 , 40);
+		clouds[2] =  new Cloud("cloudCanvas", images.Cloud, 0.08, 1.1, 0.4 , 30);
 		sunObj = new Sun("cloudCanvas", images.Sun, images.Sun2, 0.1, 1.1, 0.85, 20);
 		sunObj.draw();
 		for(k=0; k < pigs.length; k++){
 			pigs[k].draw();
 		//	objects[k].draw();
 	}
+		for(k=0; k <= horse.length; k++){
+			horse[k].draw();
+		}
 	var loopBG = setInterval(function(){
 		clouds[0].clear();
 		sunObj.draw();
