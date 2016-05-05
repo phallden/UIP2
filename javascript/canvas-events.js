a04d8a26f55c164677dd94f0886dfbdf3a96c1a5
aulander <adam.ulander@gmail.com>
2016-05-05 11:57:52 +0200

    reformat

--- javascript/canvas-events.js
+++ javascript/canvas-events.js
@@ -1,63 +1,63 @@
 var spinIt;
-$( document ).ready(function() {
- 
-	var pigCanvas = document.getElementById('pigCanvas');
-	var skyCanvas = document.getElementById('cloudCanvas');
-	var farmerCanvas = document.getElementById('farmerCanvas')
+$(document).ready(function () {
 
-	pigCanvas.addEventListener('click', function(e) {
-		console.log(pigs)
-		var mousePos = getMousePos(pigCanvas,e)
-		pigs.forEach(function(pig) {       
-			if(mousePos.x > pig.x && mousePos.x < parseInt(pig.x + pig.canvas.width() * pig.widthFact) && mousePos.y > pig.y && mousePos.y < parseInt(pig.y + (pig.canvas.width() * pig.widthFact * pig.heightFact))){
-				
-				document.getElementById('animal').style.display='block';
-			}
-		}); 
+    var pigCanvas = document.getElementById('pigCanvas');
+    var skyCanvas = document.getElementById('cloudCanvas');
+    var farmerCanvas = document.getElementById('farmerCanvas')
 
-	});
+    pigCanvas.addEventListener('click', function (e) {
+        console.log(pigs)
+        var mousePos = getMousePos(pigCanvas, e)
+        pigs.forEach(function (pig) {
+            if (mousePos.x > pig.x && mousePos.x < parseInt(pig.x + pig.canvas.width() * pig.widthFact) && mousePos.y > pig.y && mousePos.y < parseInt(pig.y + (pig.canvas.width() * pig.widthFact * pig.heightFact))) {
 
-	farmerCanvas.addEventListener('click', function(e) {
-		var mousePos = getMousePos(farmerCanvas,e)
-			if(mousePos.x > farmerObj.x && mousePos.x < parseInt(farmerObj.x + farmerObj.canvas.width() * farmerObj.widthFact) && mousePos.y > farmerObj.y && mousePos.y < parseInt(farmerObj.y + (farmerObj.canvas.width() * farmerObj.widthFact * farmerObj.heightFact))){
-				document.getElementById('farmer').style.display='block';
-			}
-	});
+                document.getElementById('animal').style.display = 'block';
+            }
+        });
 
-	pigCanvas.addEventListener('mousemove', function(e) {
-		var mousePos = getMousePos(pigCanvas,e)
-		if(mousePos.x > windObj.x && mousePos.x < parseInt(windObj.x + windObj.canvas.width() * windObj.widthFact) && mousePos.y > windObj.y && mousePos.y < parseInt(windObj.y + (windObj.canvas.width() * windObj.widthFact * windObj.heightFact))){
-			if (windObj.running == false) {
-				windObj.running = true;
-				//windObj.spin();
-				spinIt = setInterval(animateWindmill,300);
-				//windObj.raf = window.requestAnimationFrame(windObj.spin());
-			}
-		}
-		else{
-			clearInterval(spinIt);
-			windObj.running = false;
-		}
-	});
+    });
 
-	skyCanvas.addEventListener('mousemove', function(e) {
-		var mousePos = getMousePos(skyCanvas,e);
-		if(mousePos.x > sunObj.x && mousePos.x < parseInt(sunObj.x + sunObj.canvas.width() * sunObj.widthFact) && mousePos.y > sunObj.y && mousePos.y < parseInt(sunObj.y + (sunObj.canvas.width() * sunObj.widthFact * sunObj.heightFact))){
-			sunObj.imageChoice = 2;
-		}
-		else
-			sunObj.imageChoice = 1;
-	}); 
+    farmerCanvas.addEventListener('click', function (e) {
+        var mousePos = getMousePos(farmerCanvas, e)
+        if (mousePos.x > farmerObj.x && mousePos.x < parseInt(farmerObj.x + farmerObj.canvas.width() * farmerObj.widthFact) && mousePos.y > farmerObj.y && mousePos.y < parseInt(farmerObj.y + (farmerObj.canvas.width() * farmerObj.widthFact * farmerObj.heightFact))) {
+            document.getElementById('farmer').style.display = 'block';
+        }
+    });
+
+    pigCanvas.addEventListener('mousemove', function (e) {
+        var mousePos = getMousePos(pigCanvas, e)
+        if (mousePos.x > windObj.x && mousePos.x < parseInt(windObj.x + windObj.canvas.width() * windObj.widthFact) && mousePos.y > windObj.y && mousePos.y < parseInt(windObj.y + (windObj.canvas.width() * windObj.widthFact * windObj.heightFact))) {
+            if (windObj.running == false) {
+                windObj.running = true;
+                //windObj.spin();
+                spinIt = setInterval(animateWindmill, 300);
+                //windObj.raf = window.requestAnimationFrame(windObj.spin());
+            }
+        }
+        else {
+            clearInterval(spinIt);
+            windObj.running = false;
+        }
+    });
+
+    skyCanvas.addEventListener('mousemove', function (e) {
+        var mousePos = getMousePos(skyCanvas, e);
+        if (mousePos.x > sunObj.x && mousePos.x < parseInt(sunObj.x + sunObj.canvas.width() * sunObj.widthFact) && mousePos.y > sunObj.y && mousePos.y < parseInt(sunObj.y + (sunObj.canvas.width() * sunObj.widthFact * sunObj.heightFact))) {
+            sunObj.imageChoice = 2;
+        }
+        else
+            sunObj.imageChoice = 1;
+    });
 });
 
-function animateWindmill(){
-	windObj.spin();
+function animateWindmill() {
+    windObj.spin();
 }
 
 function getMousePos(canvas, evt) {
-	var rect = canvas.getBoundingClientRect();
-	return {
-		x: evt.clientX - rect.left,
-		y: evt.clientY - rect.top
-	};
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 }
